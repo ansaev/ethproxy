ad0f3702c417e5d9393abf9e84231b1c9b95d49c
Anatolii Saevskii <ansaev@gmail.com>
2021-02-26 22:33:56 +0300

    added timeout settings for eth requests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,27 +7,47 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
 const (
-	ListenAddressName     = "SERVER_ADDRESS"
-	ETHGatewayAddressName = "ETH_GATEWAY_ADDRESS"
+	envListenAddress     = "SERVER_ADDRESS"
+	envETHGatewayAddress = "ETH_GATEWAY_ADDRESS"
+	envETHTimeout        = "ETH_TIMEOUT" // in seconds
+
+	defaultTimeout int64 = 30
 )
 
 func main() {
 	if err := godotenv.Load(".env"); err != nil {
 		log.Fatal("Error loading .env file", err)
 	}
-	ethBlockService := ethfinder.New(os.Getenv(ETHGatewayAddressName), http.DefaultClient)
+
+	// parse and set timeout setting for eth requests
+	var timeout int64
+	timeoutStr := os.Getenv(envETHTimeout)
+	if timeoutStr == "" {
+		timeout = defaultTimeout
+	} else {
+		var errParsetimeout error
+		timeout, errParsetimeout = strconv.ParseInt(timeoutStr, 10, 64)
+		if errParsetimeout != nil {
+			log.Fatalf("Invalid timeout value: \"%s\", unable to parse: %s\n",
+				timeoutStr, errParsetimeout.Error())
+		}
+	}
+
+	ethBlockService := ethfinder.New(os.Getenv(envETHGatewayAddress),
+		&http.Client{Timeout: time.Duration(timeout) * time.Second})
 
 	txService := txfinder.New(ethBlockService)
 
-	apiService := api.New(os.Getenv(ListenAddressName), txService)
+	apiService := api.New(os.Getenv(envListenAddress), txService)
 	err := apiService.ListenAndServe()
 	if err != nil {
 		log.Fatal("api error: ", err)
 	}
-
 }
